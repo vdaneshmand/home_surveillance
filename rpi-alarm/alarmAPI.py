f6c41562a062f2108bae76300d09aa7b9384ecdd
vdaneshmand <v.daneshmand@gmail.com>
2017-03-28 18:12:28 -0400

    Indentations Fixed

--- rpi-alarm/alarmAPI.py
+++ rpi-alarm/alarmAPI.py
@@ -38,78 +38,78 @@ thread.daemon = False
 
 # Dictionary used to store the pin number, name, and pin state:
 pins = {
-   26 : {'name' : 'alarm', 'state' : GPIO.LOW},
-   13 : {'name' : 'siren', 'state' : GPIO.LOW},
-   19 : {'name' : 'active', 'state' : GPIO.HIGH}
-   }
+    26 : {'name' : 'alarm', 'state' : GPIO.LOW},
+    13 : {'name' : 'siren', 'state' : GPIO.LOW},
+    19 : {'name' : 'active', 'state' : GPIO.HIGH}
+    }
 
 alarm_state = {'state': 0, 'triggered': triggered}
 # Set each pin as an output and set it low:
 for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, pins[pin]['state'])
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, pins[pin]['state'])
 
 @app.route("/", methods = ['GET','POST'])
 def main():
-   """Returns alarms current state"""
+    """Returns alarms current state"""
     global alarm_state
     global triggered
     if request.method == 'POST':
-	    password = request.form.get('password')
-	    if password == 'admin':		   
-		    alarm_state['state'] = GPIO.input(13)
-		    alarm_state['triggered'] = triggered
-		    return jsonify(alarm_state)
-	    else:
-		    return 'Access Denied'
+        password = request.form.get('password')
+        if password == 'admin':		   
+            alarm_state['state'] = GPIO.input(13)
+            alarm_state['triggered'] = triggered
+            return jsonify(alarm_state)
+        else:
+            return 'Access Denied'
 		   
 @app.route("/change_state", methods = ['GET','POST'])
 def change_state():
-	"""Changes alarm's current state"""
+    """Changes alarm's current state"""
     global triggered 
     global alarm_state
     if request.method == 'POST':
-	    password = request.form.get('password')
-	    if password == 'admin':	
-	        GPIO.output(13, not GPIO.input(13))
-		    if GPIO.input(13) == 0:
-			    triggered = False			 	
-		    alarm_state['state'] = GPIO.input(13)
-		    alarm_state['triggered'] = triggered
-		    return jsonify(alarm_state)
-	    else:
-		    return 'Access Denied'
+        password = request.form.get('password')
+        if password == 'admin':	
+            GPIO.output(13, not GPIO.input(13))
+            if GPIO.input(13) == 0:
+                triggered = False			 	
+            alarm_state['state'] = GPIO.input(13)
+            alarm_state['triggered'] = triggered
+            return jsonify(alarm_state)
+        else:
+            return 'Access Denied'
 		   
 @app.route("/trigger", methods = ['GET','POST'])
 def trigger():
-   """Triggers the alarm"""
-   global triggered
-   if request.method == 'POST':
-	   password = request.form.get('password')
-	   if password == 'admin':
-		   GPIO.output(26, GPIO.HIGH)
-		   triggered = True	
-		   global thread    
+    """Triggers the alarm"""
+    global triggered
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if password == 'admin':
+            GPIO.output(26, GPIO.HIGH)
+            triggered = True	
+            global thread    
 		   
-		   if not thread.isAlive():
-			   thread = threading.Thread(name='trigger_thread', target = alarmtrigger,args=())
-			   thread.start()			   
-		   alarm_state['state'] = GPIO.input(13)
-		   alarm_state['triggered'] = triggered
+            if not thread.isAlive():
+                thread = threading.Thread(name='trigger_thread', target = alarmtrigger,args=())
+                thread.start()			   
+            alarm_state['state'] = GPIO.input(13)
+            alarm_state['triggered'] = triggered
 		   
-		   return jsonify(alarm_state)
-	   else:
-		   return 'Access Denied'
+            return jsonify(alarm_state)
+        else:
+            return 'Access Denied'
 
 def alarmtrigger():
-	global triggered
-	while True:
-		if triggered == True:	
-			 GPIO.output(26, not GPIO.input(26))
-			 time.sleep(0.25)
-		else:
-			GPIO.output(26, GPIO.LOW)
-			time.sleep(1)
+    global triggered
+    while True:
+        if triggered == True:	
+            GPIO.output(26, not GPIO.input(26))
+            time.sleep(0.25)
+        else:
+            GPIO.output(26, GPIO.LOW)
+            time.sleep(1)
   
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
